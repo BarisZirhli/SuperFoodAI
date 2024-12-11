a43097817828a0c424060ff85dccf7920f07e3f0
Enes Doğan <enesdoganlive@hotmail.com>
2024-12-12 01:11:53 +0300

    Updated react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import RecipeList from "./components/RecipeList";
 import Header from './components/Header'
 import './App.css'
+import Search from './components/Search'
 
 const recipes = [
   {
@@ -14,8 +15,9 @@ const recipes = [
 const App = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       <RecipeList recipes={recipes} />
+      <Search />
     </div>
   );
 };
