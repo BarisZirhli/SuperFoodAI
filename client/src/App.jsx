d3b460f99372e9f57a032199239b6381bf19a0ec
Enes Doğan <enesdoganlive@hotmail.com>
2024-12-18 17:32:07 +0300

    added router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
-import Header from "./components/Header";
-import RecipeCard from "./components/RecipeCard";
-import SearchBar from "./components/SearchBar";
-import './App'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Favorites from "./pages/Favorites";
+import NotFound from "./pages/NotFound";
+import "./App";
 
 function App() {
   return (
     <div className="App">
-      <RecipeList />
+      <Routes>
+        <Route path="/" element={<RecipeList />}></Route>
+        <Route path="/favorites" element={<Favorites />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Sayfa Bulunamadı</h1>
+      <p>Üzgünüz, aradığınız sayfa mevcut değil.</p>
+    </div>
+  );
+}
+
+export default NotFound;
