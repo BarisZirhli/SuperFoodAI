29efe18763b6512ea7da7e35c12973f9e0202ded
BarisZirhli <bariszirhli10@gmail.com>
2024-12-23 18:17:01 +0300

    newdbC

--- server/migrations/20241223103322-create-rating.js
+++ server/migrations/20241223103322-create-rating.js
@@ -2,50 +2,58 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Ratings", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      UserId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Users",
-          key: "id"
+    await queryInterface.createTable(
+      "Ratings",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
         },
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE"
-      },
-      RecipeId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Recipes",
-          key: "id"
+        UserId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Users",
+            key: "id"
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE"
         },
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE"
-      },
-      Rating: {
-        type: Sequelize.FLOAT,
-        allowNull: false,
-        validate: {
-          min: 0,
-          max: 5
+        RecipeId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Recipes",
+            key: "id"
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE"
+        },
+        Rating: {
+          type: Sequelize.FLOAT,
+          allowNull: false,
+          validate: {
+            min: 0,
+            max: 5
+          }
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
         }
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+      {
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false
       }
-    });
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Ratings");
