03fdf1a0ddb8752994cb8519b8ede44962fcb0cd
Enes Doğan <enesdoganlive@hotmail.com>
2024-12-26 12:59:16 +0300

    implement login operations

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,5 +1,8 @@
 const user = require("../models/user");
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+
+
 const generateToken = (payload) =>{
 return jwt.sign(payload,"ai-based-food-recommendation-system",{
     expiresIn: "60m",
@@ -37,4 +40,31 @@ const signup = async (req, res, next) => {
   });
 };
 
-module.exports = { signup };
+const login = async(req,res,next) =>{
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        res.status(400).json({
+            status : 'fail',
+            message : "Incorrect email or password!"
+        })
+    }
+
+    const loginResult = await user.findOne({ where: { email } });
+    if(!loginResult || !(await bcrypt.compare(password,loginResult.password))){
+       return  res.status(401).json({
+            status : 'fail',
+            message : "Incorrect email or password"
+        });
+    }
+    const token = generateToken({
+        id : loginResult.id,
+    })
+    return res.status(200).json({
+        status : 'success',
+        message : 'User logged in successfully',
+        token,
+    })
+}
+
+module.exports = { signup,login };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,10 @@
-const { signup } = require('../controller/authController');
+const { signup } = require("../controller/authController");
+const { login } = require("../controller/authController");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.route('/signup').post(signup);
+router.route("/signup").post(signup);
+
+router.route("/login").post(login);
 
 module.exports = router;
