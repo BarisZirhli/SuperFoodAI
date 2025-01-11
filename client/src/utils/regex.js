3185bc9957a7a4414cbbf79d477a296898a5d392
Enes Doğan <enesdoganlive@hotmail.com>
2025-01-11 19:57:51 +0300

    Update regex

--- client/src/utils/regex.js
+++ client/src/utils/regex.js
@@ -1,23 +1,27 @@
 export const parseIngredients = (ingredients) => {
   return ingredients
-    .replace(/C\s*\(/, "")
-    .replace(")", "")
-    .split(",")
-    .map((item) => item.replace(/"/g, "").trim());
+    .replace(/C\s*\(/, "") // "C (" kısmını kaldır
+    .replace(")", "") // Sonundaki ")" kısmını kaldır
+    .split(",") // Virgüllere göre ayır
+    .map((item) => `𐤟 ${item.replace(/"/g, "").trim()}`); // Her bir öğenin başına 𐤟 ekle
 };
+
 
 export const parseInstructions = (instructions) => {
   return instructions
-    .replace(/^C\s*\(/, "")
-    .replace(/\)$/, "")
-    .split(/",\s*"/)
-    .map((step) => step.replace(/"/g, "").trim());
+    .replace(/^C\s*\(/, "") // Başındaki "C (" kısmını kaldır
+    .replace(/\)$/, "") // Sonundaki ")" kısmını kaldır
+    .replace(/,\s*$/, "") // Cümle sonlarındaki gereksiz virgülleri kaldır
+    .replace(/([a-zA-Z0-9])\./g, "$1.") // Noktalama hatalarını düzelt
+    .split(/\.(?!\s*\d)/) // Noktalara göre ayır
+    .map((step) => step.trim()) // Başlangıç ve son boşlukları temizle
+    .filter((step) => step.length >= 1); // Boş maddeleri kaldır
 };
 
 export const parseImageUrls = (image) => {
   return image
-    .replace(/^c\(/, "")
-    .replace(/\)$/, "")
-    .split(/",\s*"/)
-    .map((url) => url.replace(/"/g, "").trim());
+    .replace(/^c\(/, "") // "c(" ifadesini kaldır
+    .replace(/\)$/, "") // Kapanış parantezini kaldır
+    .split(/,\s*(?=https?:\/\/)/) // URL'leri virgülle ayır
+    .map((url) => url.trim()); // Her URL'yi temizle
 };
